b1da6672716eff8727a93a0c9e54bce80eeaab89
Nishant Saini <nishantsaini021.work@gmail.com>
2026-07-03 20:09:02 +0530

    Recursion Excercise

--- 05(Functions)/Practice.py
+++ 05(Functions)/Practice.py
@@ -48,7 +48,6 @@
 #     for num in numbers:
 #         count += 1
 #     return count
-
 # print(find_max(numbers))
 # print(sum_list(numbers))
 # print(count_total_numbers(numbers))
@@ -64,10 +63,29 @@
 # print(sum_n(n))
 
 
-#### Find the sum of digits of a number recursively:
-n = 1256
-def fun_sum(n):
-    if n // 10 == 0:
-        return n
-    return((n % 10) + fun_sum(n // 10))
-print(fun_sum(n))
+# #### Find the sum of digits of a number recursively:
+# n = int(input("Enter a Number: "))
+# def fun_sum(n):
+#     if n // 10 == 0:
+#         return n
+#     return((n % 10) + fun_sum(n // 10))
+# print(fun_sum(n))
+
+
+#### Find The Number of Digits in a Given Number
+# n = int(input("Enter a Number: "))
+# def fun_sum(n):
+#     if n // 10 == 0:
+#         return 1
+#     return( 1 + fun_sum(n // 10))
+# print(fun_sum(n))
+
+#### Reverse a String Using Recursion
+n = input("Enter a String to Reverse: ")
+x = len(n) - 1
+def fun_reverse(n):
+    if len(n) == 1:
+        return n 
+    return fun_reverse(n[1:]) + n[0]
+
+print(fun_reverse(n))
